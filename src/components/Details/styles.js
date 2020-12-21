e69718c0e85878b5bd06f57da2f0053b436cdff2
Boro23 <404slimboysavage@gmail.com>
2020-12-21 19:48:23 +0530

    first commit

---
+++ src/components/Details/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core';
+
+export default makeStyles(() => ({
+  income: {
+    borderBottom: '10px solid rgba(214, 243, 121, 1)',
+  },
+  expense: {
+    borderBottom: '10px solid rgba(226, 76, 109, 1)',
+  },
+}));
